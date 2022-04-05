8d0946d459fdff2ce1b24c45559f3cff082467ea
unknown <anilamanele89@gmail.com>
2022-04-05 22:56:51 +0700

    Latihan4

---
+++ application/views/V_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Mariyani</li>
+        <li>Nama Belakang : Lamanele</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Komplek uka blok bj, no.08 RT017/008</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Yoga</li>
+        <li>Zumba</li>
+    </ul>
+</section>
